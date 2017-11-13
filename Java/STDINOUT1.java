c4328a0f7bbe9ea074934c3149436f1f9482736e
abhisekjuneja <abhisekjuneja@gmail.com>
2017-11-13 11:55:23 +0530

    Tried to Fix Scrollbar Issue - Attempt 1

--- Java/STDINOUT1.java
+++ Java/STDINOUT1.java
@@ -1,16 +1,16 @@
 /**
  * Solution To : "Java STDIN and STDOUT I"
- * Problem Statement: https://www.hackerrank.com/rest/contests/master/challenges/java-stdin-and-stdout-1/download_pdf?language=English
+ * Problem Statement: https://goo.gl/XtfdHD
  */
 
 import java.util.Scanner;
 
 public class STDINOUT1 {
-public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        for(int i = 0; i < 3; i++) {
-                int numberFromSTDIN = scan.nextInt();
-                System.out.println(numberFromSTDIN);
-        }
-}
+ public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+  for (int i = 0; i < 3; i++) {
+   int numberFromSTDIN = scan.nextInt();
+   System.out.println(numberFromSTDIN);
+  }
+ }
 }
